3bbbfd01396a1fbe6766e7dcc2a6d3ef195ed5d7
Stephen Asamoah <stephenasamoah@users.noreply.github.com>
2019-10-18 14:55:58 +0000

    #### Movies List - Added helper classes to scss - Fixed out-of-control image sizes on cards display
    #### Header
    - Added logic handling on when user exists for CTA's on header

--- src/app/ui/header/header.component.ts
+++ src/app/ui/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SearchUtilService } from '../../api/_utilities/search-util.service';
+import { User } from '../../api/_models/user.model';
 
 @Component({
   selector: 'hf-header',
@@ -7,8 +8,10 @@ import { SearchUtilService } from '../../api/_utilities/search-util.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  user: User;
 
   constructor(private sus: SearchUtilService) {
+    // this.user = {};
   }
 
   ngOnInit() {
